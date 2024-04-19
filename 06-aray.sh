785178c0cf640dd4f9adcb8d6024d029f9e04547
DevPhani23 <devphani23600@gmail.com>
2024-04-19 16:02:59 +0530

    arrays, basic program

---
+++ 06-aray.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MOVIES=("RRR" "DDL" "JVAS")
+
+# size of above array is 3,
+# Index arre 0,1,2
+
+# list always starts with 0,
+
+echo "First Movie is : ${MOVIES[0]}"
+echo "Second Movie is : ${MOVIES[0]}"
+echo "Third Movie is : ${MOVIES[0]}"
