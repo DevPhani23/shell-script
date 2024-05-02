39badd6269655f5bc84dd3d1d0c4fac7164b28ad
DevPhani23 <devphani23600@gmail.com>
2024-05-02 21:07:14 +0530

    devops with aws

--- 12-colors.sh
+++ 12-colors.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE+/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 R="\e[31m" #Red
 G="\e[32"  #Green
@@ -12,9 +12,9 @@ N="\e[0m"  #Normal color
 echo "Script started executing at: $TIMESTAMP"
 
 VALIDATE(){
-    if [$1 -ne 0]
+    if [ $1 -ne 0 ]
     then
-        echo -e "$2....$R FAILURE $N"
+        echo -e "$2....$R...FAILURE $N"
         exit 1
     else
         echo -e "$2....$G SUCCESS $N"
